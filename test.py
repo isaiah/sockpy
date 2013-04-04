88253508384ccf4c62ef63e80356b984442a8cbc
Isaiah Peng <isaiah.peng@vcint.com>
2013-04-04 16:57:09 +0200

    change for send reply

--- test.py
+++ test.py
@@ -8,25 +8,26 @@ class Tester(object):
   """
   Use this class to flood your vertx sockjs server with websocket connections
   """
-  def __init__(self, host, address, max_conn):
+  def __init__(self, host, address, max_conn, prefix):
     self.interval = 0.01
     self.host = host
     self.address = address
     self.max_conn = max_conn
+    self.prefix = prefix
     self.conns = []
     self.lc = task.LoopingCall(self.new_conn)
     self.lc.start(self.interval)
 
   def new_conn(self):
     if len(self.conns) <= self.max_conn:
-      eb = EventBus("http://{}/eventbus".format(self.host))
+      eb = EventBus("http://{}/{}".format(self.host, self.prefix))
       self.conns.append(eb)
       def onopen(message):
-        print "onopen"
+        print len(self.conns)
         eb.registerHandler(self.address, handler)
 
       def handler(message, replyTo):
-        print(message)
+          eb.send("acknowledge", "1")
 
       eb.addEventListener("open", onopen)
       threads.deferToThread(eb.connect)
@@ -37,5 +38,6 @@ if __name__ == "__main__":
     host = conf["host"]
     address = conf["address"].encode("utf8")
     max_conn = conf["socks"]
-    tester = Tester(host, address, max_conn)
+    prefix = conf["prefix"]
+    tester = Tester(host, address, max_conn, prefix)
   reactor.run()
